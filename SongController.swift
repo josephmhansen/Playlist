1608a7218097e0cb66cda258c1e3e2939eade27a
Joseph Hansen <theythemtheir@icloud.com>
2016-07-07 13:53:51 -0600

    created SongController

---
+++ SongController.swift
@@ -0,0 +1,16 @@
+//
+//  SongController.swift
+//  Playlist2
+//
+//  Created by Joseph Hansen on 7/7/16.
+//  Copyright © 2016 Joseph Hansen. All rights reserved.
+//
+
+import Foundation
+
+class SongController {
+    func creatSong(name: String, artist: String, playlist: Playlist) {
+        let song = Song(title: name, artist: artist)
+        PlaylistController.sharedController.addSongToPlaylist(song, playlist: playlist)
+    }
+}
