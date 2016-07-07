96dd62c5fd6e03ce0f5588a8d57781af2ee1dd21
Joseph Hansen <theythemtheir@icloud.com>
2016-07-07 12:12:29 -0600

    updated playlist controller

---
+++ PlaylistController.swift
@@ -0,0 +1,29 @@
+//
+//  PlaylistController.swift
+//  Playlist2
+//
+//  Created by Joseph Hansen on 7/7/16.
+//  Copyright © 2016 Joseph Hansen. All rights reserved.
+//
+
+import Foundation
+
+class PlaylistController {
+    //CRUD (CREATE, READ, UPDATE, DELETE)
+    //    READ
+    var playlists: [Playlist] = []
+    //    CREATE
+    func addPlaylist(name: String) {
+        let playlist = Playlist(title: name)
+        playlists.append(playlist)
+    }
+    
+    //    DELETE
+    func deletePlaylist(playlist: Playlist) {
+        if let index = playlists.indexOf(playlist){
+            playlists.removeAtIndex(index)
+        }
+        
+        
+    }
+}
