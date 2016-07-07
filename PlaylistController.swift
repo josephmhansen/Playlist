a05d86d68eb883870ca60f55743c4c36ddf7e391
Joseph Hansen <theythemtheir@icloud.com>
2016-07-07 13:44:59 -0600

    completed PlaylistController

--- PlaylistController.swift
+++ PlaylistController.swift
@@ -26,7 +26,15 @@ class PlaylistController {
         if let index = playlists.indexOf(playlist){
             playlists.removeAtIndex(index)
         }
-        
-        
     }
+    func addSongToPlaylist(song: Song, playlist: Playlist) {
+        playlist.songsArray.append(song)
+    }
+    func removeSongFromPlaylist(song: Song, playlist: Playlist) {
+        guard let index = playlist.songsArray.indexOf(song) else {
+            return
+        }
+        playlist.songsArray.removeAtIndex(index)
+    }
+    
 }
